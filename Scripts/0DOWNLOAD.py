867cd3bd70fe22c52940488d3b402f8bdb2996e6
Mr-Horse <30724224+Mr-Horse@users.noreply.github.com>
2018-08-25 12:52:31 +0200

    Fixed error handling
    added a 404 exception if the post gets deleted

--- Scripts/0DOWNLOAD.py
+++ Scripts/0DOWNLOAD.py
@@ -9,9 +9,9 @@ import shutil # shell utilities
 
 
 path = config.path
-subreddit_name = config.subreddit_name
 download_limit = config.download_limit
 sleeptime = config.sleeptime_download
+configsubreddit_name = config.subreddit_name
 
 def main(subreddit_name, current_dir = os.getcwd()):
     reddit = praw.Reddit(
@@ -21,24 +21,29 @@ def main(subreddit_name, current_dir = os.getcwd()):
         password = config.password, 
         user_agent = 'subreddit crawler')
 
+    #dir_path = '/home/pi/Desktop/python/me_irl/'
     if not os.path.exists(path):
         os.mkdir(path)
 
-    subreddit = reddit.subreddit(subreddit_name).hot(limit= int(download_limit))
+    subreddit = reddit.subreddit(configsubreddit_name).hot(limit= config.download_limit)
 
-    print("Downloading files...")
+    print("Download files...")
 
     for submissions in subreddit:
         if not submissions.stickied and submissions.score > 50:
             fullfilename = os.path.join(path, "{}.jpg".format(submissions))
             request = web.Request(submissions.url, headers = {'User-Agent':'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:52.0) Gecko/20100101 Firefox/52.0'})
-            with web.urlopen(request) as response, open(fullfilename, 'wb') as out_file:                
-                try:
-                    shutil.copyfileobj(response, out_file)
-                except HTTPError as e:
-                    print("{}{}{}".format(Fore.RED, e.code, Style.RESET_ALL))
-                except URLError as e:
-                    print("{}{}{}".format(Fore.RED, e.reason, Style.RESET_ALL))                 
+            try:
+                with web.urlopen(request) as response, open(fullfilename, 'wb') as out_file:                
+                    try:
+                        shutil.copyfileobj(response, out_file)
+                    except HTTPError as e:
+                        print("{}{}{}".format(Fore.RED, e.code, Style.RESET_ALL))
+                    except URLError as e:
+                        print("{}{}{}".format(Fore.RED, e.reason, Style.RESET_ALL)) 
+            except HTTPError:
+                print('HTTPError, skipping image...')
+                continue
 
             # delete corrupted files smaller than 55 KB
             filesize = os.path.getsize(fullfilename)
@@ -56,8 +61,8 @@ def main(subreddit_name, current_dir = os.getcwd()):
 	
 
 if __name__ == '__main__':
-    main(subreddit_name)
+    main(configsubreddit_name)
 
 while True:
-        main(subreddit_name, current_dir = os.getcwd())
+        main(configsubreddit_name, current_dir = os.getcwd())
 
